9cdbb40d9f307d93a4c6b5066b109d92d3b25667
Bryan DeGrendel <bryan@degrendel.com>
2024-01-29 23:47:27 -0500

    Change Kotlin Json dependency to API

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
 
 java {
