72399595f6cd0f01a97063cfef095874977b8156
Bryan DeGrendel <bryan@degrendel.com>
2024-03-26 18:28:38 -0400

    Add configuration for internal Maven repository

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -33,10 +33,62 @@ java {
     withSourcesJar()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
+if (project.hasProperty("internalMavenURL"))
+{
+    val internalMavenUsername: String by project
+    val internalMavenPassword: String by project
+    val internalMavenURL: String by project
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+        }
+        repositories {
+            maven {
+                credentials {
+                    username = internalMavenUsername
+                    password = internalMavenPassword
+                }
+                val releasesRepoUrl = "$internalMavenURL/releases/"
+                val snapshotsRepoUrl = "$internalMavenURL/snapshots/"
+                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+                name = "Internal-Maven-Publish"
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            credentials {
+                username = internalMavenUsername
+                password = internalMavenPassword
+            }
+            url = uri("$internalMavenURL/releases")
+            name = "Internal-Maven-Releases"
+        }
+    }
+
+    repositories {
+        maven {
+            credentials {
+                username = internalMavenUsername
+                password = internalMavenPassword
+            }
+            url = uri("$internalMavenURL/snapshots")
+            name = "Internal-Maven-Snapshots"
+        }
+    }
+} else {
+    repositories {
+        mavenLocal()
+    }
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
         }
     }
 }
