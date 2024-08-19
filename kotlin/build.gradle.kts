44d96a2e1ad92a2a3e2d1168881ec02b32ae7eaf
Bryan DeGrendel <bryan@degrendel.com>
2024-08-18 21:03:30 -0400

    Bump to latest JSON schema plugin

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("net.pwall.json:json-kotlin-gradle:0.96.1")
+        classpath("net.pwall.json:json-kotlin-gradle:0.107")
     }
 }
 
